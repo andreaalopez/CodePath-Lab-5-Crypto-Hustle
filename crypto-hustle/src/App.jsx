7eca823acbff45875d3f4da174ca66775aad95d4
Andrea Lopez <apl2171@barnard.edu>
2023-10-14 14:58:12 -0400

    lab progress

---
+++ crypto-hustle/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import CoinInfo from "./components/CoinInfo";
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+function App() {
+  const [list, setList] = useState(null)
+
+  useEffect(() => {
+
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY
+      );
+
+      const json = await response.json();
+      setList(json);
+    };
+
+    fetchAllCoinData().catch(console.error);
+  }, [])
+
+
+  return (
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <ul>
+        {list && Object.entries(list.Data).map(([coin]) =>
+          list.Data[coin].PlatformType === "blockchain" ? (
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
+          ) : null
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
